cc08d65d26f0b344e180f556d5c0384de5062a67
CkYes-DBT <23502410026@student.prasetiyamulya.ac.id>
2026-04-21 17:53:27 +0700

    modelData_Weather

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -15,3 +15,29 @@ class WeatherApp extends StatelessWidget {
         home: const WeatherPage(),
       );
 }
+
+// Data Model
+class WeatherData {
+  final String city, country, description, icon, main;
+  final double temp, feelsLike, windSpeed;
+  final int humidity;
+
+  const WeatherData({
+    required this.city, required this.country, required this.description,
+    required this.icon, required this.main, required this.temp,
+    required this.feelsLike, required this.windSpeed, required this.humidity,
+  });
+
+  factory WeatherData.fromJson(Map<String, dynamic> j) => WeatherData(
+        city: j['name'], country: j['sys']['country'],
+        description: j['weather'][0]['description'],
+        icon: j['weather'][0]['icon'],
+        main: j['weather'][0]['main'],
+        temp: (j['main']['temp'] as num).toDouble(),
+        feelsLike: (j['main']['feels_like'] as num).toDouble(),
+        windSpeed: (j['wind']['speed'] as num).toDouble(),
+        humidity: j['main']['humidity'],
+      );
+}
+
+
